eeb629674c6c6458d5d32e5c396a6d165653bc5c
ama <happy.ama89@gmail.com>
2017-07-20 16:15:14 +0900

    Action controller 추가

---
+++ src/sulbinjung/controller/Action.java
@@ -0,0 +1,9 @@
+package sulbinjung.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class Action {
+	//추상메소드
+	public abstract ActionForward execute(HttpServletRequest request, HttpServletResponse response);
+}
