30aa213944e62013680962c96e33e92a340ff4a1
wonyoung <shinho0505@naver.com>
2017-07-21 17:45:04 +0900

    memberdto, dao, mapper fixed

--- src/sulbinjung/controller/UserActionFactory.java
+++ src/sulbinjung/controller/UserActionFactory.java
@@ -1,7 +1,16 @@
 package sulbinjung.controller;
 
 import sulbinjung.admin.action.AdminLoginAction;
-import sulbinjung.users.action.HomeAction;
+import sulbinjung.members.action.CheckIdAction;
+import sulbinjung.members.action.HomeAction;
+import sulbinjung.members.action.MembersDeleteAction;
+import sulbinjung.members.action.MembersInfoAction;
+import sulbinjung.members.action.MembersUpdateAction;
+import sulbinjung.members.action.MembersUpdateformAction;
+import sulbinjung.members.action.SigninAction;
+import sulbinjung.members.action.SigninFormAction;
+import sulbinjung.members.action.SignoutAction;
+import sulbinjung.members.action.SignupAction;
 
 public class UserActionFactory {
 	private static UserActionFactory factory;
@@ -21,6 +30,24 @@ public class UserActionFactory {
 			action=new HomeAction();
 		}else if(command.equals("/admin/login")){
 			action=new AdminLoginAction();
+		}else if(command.equals("/members/checkid")){
+			action=new CheckIdAction();
+		}else if(command.equals("/members/signup")){
+			action=new SignupAction();
+		}else if(command.equals("/members/signin_form")){	
+			action=new SigninFormAction();
+		}else if(command.equals("/members/signin")){
+			action=new SigninAction();
+		}else if(command.equals("/members/signout")){
+			action=new SignoutAction();
+		}else if(command.equals("/members/private/info")){
+			action=new MembersInfoAction();
+		}else if(command.equals("/members/private/delete")){
+			action=new MembersDeleteAction();
+		}else if(command.equals("/members/private/updateform")){
+			action=new MembersUpdateformAction();
+		}else if(command.equals("/members/private/update")){
+			action=new MembersUpdateAction();
 		}
 		return action;
 	}
--- src/sulbinjung/members/action/HomeAction.java
+++ src/sulbinjung/members/action/HomeAction.java
@@ -1,4 +1,4 @@
-package sulbinjung.users.action;
+package sulbinjung.members.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
