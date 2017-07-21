cfe118bf39bbf977cdc9c95f063b386bc2580b95
wonyoung <shinho0505@naver.com>
2017-07-21 17:46:17 +0900

    memberdto, dao, mapper fixed

---
+++ src/sulbinjung/members/action/CheckIdAction.java
@@ -0,0 +1,17 @@
+package sulbinjung.members.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sulbinjung.controller.Action;
+import sulbinjung.controller.ActionForward;
+
+public class CheckIdAction extends Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
